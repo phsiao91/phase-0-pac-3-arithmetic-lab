12f17e15d3da42585c30f00789bd18bfc288fd06
Phing An Hsiao <phinganhsiao@gmail.com>
2021-07-03 13:47:56 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+function increment(number1) {
+    return ++number1;
+}
+
+function decrement(number1) {
+    return --number1;
+}
+
+function makeInt(stringValue) {
+    return parseInt(stringValue, 10);
+}
+
+function preserveDecimal(stringValue) {
+    return parseFloat(stringValue);
+}
